702178e6fb5b3f9cfd2ba0e1830562a90a51dbdb
lf2013 <luoluo1920@163.com>
2013-05-17 15:03:39 +0800

    rotate

--- redBlackTree/rbTree.h
+++ redBlackTree/rbTree.h
@@ -3,3 +3,43 @@ struct rbTree{
 	int color;
 	int k;
 };
+
+void
+leftRotate(struct rbTree * x)
+{
+	struct rbTree * y = x -> r;
+	struct rbTree * z = y -> l;
+	x -> r = z;
+	if(z != NULL)
+		z -> p = x;
+	
+	y -> p = x -> p;
+	if(x -> p != NULL)
+		if(x -> p -> l == x)
+			x -> p -> l = y;	
+		else 
+			x -> p -> r = y;
+	
+	y -> l = x;
+	x -> p = y;
+}
+
+void
+rightRotate(struct rbTree * x)
+{
+	struct rbTree * y = x -> l;
+	struct rbTree * z = y -> r;
+	x -> l = z;
+	if(z != NULL)
+		z -> p = x;
+	
+	y -> p = x -> p;
+	if(x -> p != NULL)
+		if(x -> p -> l == x)
+			x -> p -> l = y;	
+		else 
+			x -> p -> r = y;
+	
+	y -> r = x;
+	x -> p = y;
+}
